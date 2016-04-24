5fd6a3757e14d2b73de120c5ff58bb46689b8f06
zunda <zundan@gmail.com>
2016-04-23 17:58:26 -1000

    Specify timezones

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module RailsTimez
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    ENV['TZ'] = 'Japan'
+    config.time_zone = 'UTC'
   end
 end
